110bb3bdd7936bb5446428ad8b1bb490ae9b74db
Sergey <SergeyVorobevWork@gmail.com>
2017-05-17 14:59:18 +0300

    fixed collision bug with peshka

--- painter/game.cpp
+++ painter/game.cpp
@@ -8,20 +8,24 @@ std::vector<Figure *> Game::figures;
 int Game::gameOver = 0;
 bool Game::clicked = false;
 int  Game::temporaryFig = -1;
+ bool flag = true;
 void Game::check_over(){
     for(int i = 0; i < Game::figures.size();++i){
         if(Game::figures[i]->check()){
             for(int j = 0; j < Game::figures.size();++j){
-                if(Game::figures[i]->getCol() == Game::figures[j]->getCol() && !Game::figures[j]->can_go()){
-                    Game::gameOver = (!Game::figures[i]->getCol())+1;
-                    return;
+                if(Game::figures[i]->getCol() == Game::figures[j]->getCol() && Game::figures[j]->can_go()){
+                    flag = false;
                 }
             }
+            if(flag){
+                Game::gameOver = (!Game::figures[i]->getCol())+1;
+                return;
+            }
         }
+
     }
-    bool flag = true;
     for(int  i = 0; i < Game::figures.size();++i){
-        if(figures[i]->can_go()){
+        if(figures[i]->can_go() && figures[i]->getCol() != turn){
             flag = false;
         }
     }
